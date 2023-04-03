fab626e08ff680ee10adf4c31c93426e595765a6
Justin Kam <gamhs99@gmail.com>
2023-04-03 23:50:17 +0900

    수정

--- app/src/main/java/com/example/registerloginexample/ListActivity.java
+++ app/src/main/java/com/example/registerloginexample/ListActivity.java
@@ -1,14 +1,43 @@
 package com.example.registerloginexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListActivity extends AppCompatActivity {
 
+
+    private Button btn_sell2, btn_main; // 판매하기, 취소
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        btn_sell2 = findViewById(R.id.btn_sell2); // 판매하기 
+        btn_main = findViewById(R.id.btn_main); // 메인
+
+
+        btn_sell2.setOnClickListener(new View.OnClickListener() {    // 판매하기
+            @Override
+            public void onClick(View view) {
+                Intent sellContinue_intent = new Intent(ListActivity.this, SellActivity.class);
+                startActivity(sellContinue_intent);
+
+            }
+        });
+
+        btn_main.setOnClickListener(new View.OnClickListener() {    // 메인
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
-}
+
+
+
+    }
+
