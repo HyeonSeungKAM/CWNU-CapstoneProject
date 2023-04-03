5b708f7415320a1452ed86fbadd515ab15aa65c7
Justin Kam <gamhs99@gmail.com>
2023-04-03 22:15:53 +0900

    수정

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.registerloginexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_sell, btn_list; // 판매하기, 목록 버튼
+
 
     // 서버에서 가져온 내용 보여주기
     @Override
@@ -31,6 +37,30 @@ public class MainActivity extends AppCompatActivity {
         tv_name.setText(userName);
         tv_glassW.setText(glass);
         tv_binName.setText(binName);
+
+
+
+        btn_sell = findViewById(R.id.btn_sell); // 판매하기
+        btn_list = findViewById(R.id.btn_list); // 목록
+        btn_sell.setOnClickListener(new View.OnClickListener() {    // 판매하기
+            @Override
+            public void onClick(View view) {
+                Intent SELLintent = new Intent(MainActivity.this, SellActivity.class);
+                startActivity(SELLintent);
+
+            }
+        });
+
+        btn_list.setOnClickListener(new View.OnClickListener() {    // 목록
+            @Override
+            public void onClick(View view) {
+                Intent LISTintent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(LISTintent);
+
+            }
+        });
+
+
 
     }
 
---
+++ app/src/main/java/com/example/registerloginexample/SellActivity.java
@@ -0,0 +1,15 @@
+package com.example.registerloginexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SellActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell);
+
+
+    }
+}
